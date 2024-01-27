0a989eb3a7c51b7a079d6a34e7f60443dbba5e75
Abegail Isidro <chabbyabe@gmail.com>
2024-01-27 19:25:43 +0900

    Fix migration order

--- database/migrations/2024_01_21_074444_create_wallets_table.php
+++ database/migrations/2024_01_21_074444_create_wallets_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+
         Schema::create('wallets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
