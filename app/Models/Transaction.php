6a57b675544f9cf57eb24286f1fec6a82a42a3f1
Abegail Isidro <chabbyabe@gmail.com>
2024-01-27 18:53:08 +0900

    Add user relationship to transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -11,4 +12,13 @@ class Transaction extends Model
 
     protected $fillable = [ 'description', 'amount'];
 
+    /**
+     * Get the user that owns the Transaction
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
